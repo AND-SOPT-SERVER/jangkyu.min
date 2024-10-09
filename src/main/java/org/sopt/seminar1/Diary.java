055bb2efb750c6e59f3da79a26e1f8bba39265ce
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-10 06:09:48 +0900

    [Seminar1] 하루에 하나의 일기만 작성할 수 있도록 수정

--- src/main/java/org/sopt/seminar1/Diary.java
+++ src/main/java/org/sopt/seminar1/Diary.java
@@ -1,12 +1,21 @@
 package org.sopt.seminar1;
 
-public class Diary {
-    private Long id;
-    private final String body;
+import java.time.LocalDate;
 
-    public Diary(Long id, String body) {
+public class Diary {
+    private Long id = null;
+    private final String body;
+    private final LocalDate writeTime;
+
+    public Diary(Long id, String body, LocalDate writeTime) {
         this.id = id;
         this.body = body;
+        this.writeTime = writeTime;
+    }
+
+    public Diary(String body, LocalDate writeTime) {
+        this.body = body;
+        this.writeTime = writeTime;
     }
 
     public Long getId() {
@@ -17,4 +26,7 @@ public class Diary {
         return this.body;
     }
 
+    public LocalDate getWriteTime() {
+        return writeTime;
+    }
 }
