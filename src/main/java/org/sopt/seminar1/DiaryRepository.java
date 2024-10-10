70970ddb58040a7971c11d280526ef989c54578c
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-10 16:30:17 +0900

    [Seminar1] 일기가 계속해서 저장 및 관리될 수 있도록 파일 입출력 사용

--- src/main/java/org/sopt/seminar1/DiaryRepository.java
+++ src/main/java/org/sopt/seminar1/DiaryRepository.java
@@ -1,5 +1,7 @@
 package org.sopt.seminar1;
 
+import java.io.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -91,5 +93,63 @@ public class DiaryRepository {
     final void restore(Long id) {
         storage.put(id, trashStorage.get(id));
         trashStorage.remove(id);
+    }
+
+    private void saveMap(Map<Long, Diary> map, String filePath, long size) throws IOException {
+        FileWriter writer = new FileWriter(filePath);
+
+        for(long index = 1; index <= size; index++) {
+            final Diary diary = map.get(index);
+
+            if(diary != null) {
+                String data = index + " " + diary.getBody() + " " + diary.getWriteTime();
+                writer.write(data);
+                writer.write("\n");
+            }
+        }
+        writer.close();
+    }
+
+    public void saveStorage() throws IOException {
+        saveMap(storage, "src/main/java/org/sopt/seminar1/storage/diary.txt", numbering.longValue());
+    }
+
+    public void saveTrashStorage() throws IOException {
+        saveMap(trashStorage, "src/main/java/org/sopt/seminar1/storage/trash.txt", numbering.longValue());
+    }
+
+    public void savePatchStorage() throws IOException {
+        saveMap(patchStorage, "src/main/java/org/sopt/seminar1/storage/patch.txt", numberingPatch.longValue());
+    }
+
+    private void loadMap(Map<Long, Diary> map, String filePath, boolean isPatch) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(" ");
+            long id = Long.parseLong(parts[0]);
+            String body = parts[1];
+            LocalDate writeTime = LocalDate.parse(parts[2]);
+
+            map.put(id, new Diary(id, body, writeTime));
+
+            if(isPatch) {
+                numberingPatch.set(Math.max(numberingPatch.longValue(), id));
+            } else {
+                numbering.set(Math.max(numbering.longValue(), id));
+            }
+        }
+    }
+
+    public void loadStorage() throws IOException {
+        loadMap(storage, "src/main/java/org/sopt/seminar1/storage/diary.txt", false);
+    }
+
+    public void loadTrashStorage() throws IOException {
+        loadMap(trashStorage, "src/main/java/org/sopt/seminar1/storage/trash.txt", false);
+    }
+
+    public void loadPatchStorage() throws IOException {
+        loadMap(patchStorage, "src/main/java/org/sopt/seminar1/storage/patch.txt", true);
     }
 }
