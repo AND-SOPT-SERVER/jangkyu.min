4e836ecedb11b9e41baa82adb829531236958a19
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-10 16:30:17 +0900

    [Seminar1] 일기가 계속해서 저장 및 관리될 수 있도록 파일 입출력 사용

--- src/main/java/org/sopt/seminar1/DiaryService.java
+++ src/main/java/org/sopt/seminar1/DiaryService.java
@@ -1,5 +1,6 @@
 package org.sopt.seminar1;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -102,5 +103,17 @@ public class DiaryService {
         } else {
             System.err.println("휴지통에 존재하지 않는 다이어리 입니다.");
         }
+    }
+
+    void saveDiary() throws IOException {
+        diaryRepository.saveStorage();
+        diaryRepository.saveTrashStorage();
+        diaryRepository.savePatchStorage();
+    }
+
+    void loadDiary() throws IOException {
+        diaryRepository.loadStorage();
+        diaryRepository.loadTrashStorage();
+        diaryRepository.loadPatchStorage();
     }
 }
