e3ee4a1240e343f7743487eaa143194aa7bd6910
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-29 23:41:37 +0900

    [Feat] 예외처리 수정 및 목록 조회 기능 수정

--- src/main/java/org/sopt/diary/repository/DiaryRepository.java
+++ src/main/java/org/sopt/diary/repository/DiaryRepository.java
@@ -3,6 +3,9 @@ package org.sopt.diary.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
+    List<DiaryEntity> findTop10ByOrderByIdDesc();
 }
