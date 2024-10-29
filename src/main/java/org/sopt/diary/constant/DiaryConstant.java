b441d517bb981b8ebb98ec12ce37f469d6a45cef
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-29 17:55:49 +0900

    [Feat] 다이어리의 길이를 enum 타입으로 수정

---
+++ src/main/java/org/sopt/diary/constant/DiaryConstant.java
@@ -0,0 +1,10 @@
+package org.sopt.diary.constant;
+
+public class DiaryConstant {
+    public static final Integer MAX_DIARY_LENGTH = 30;
+
+    // 생성자를 private 선언하는 이유 : 외부에서 인스턴스화하지 못하도록 방지
+    private DiaryConstant() {
+        // 인스턴스화 방지 목적이기에 아무 내용도 필요 없음
+    }
+}
