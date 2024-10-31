871ac769adf5b36a3ed57bffa3e7d2eba89288fb
Jang99u <alswkdrb@dgu.ac.kr>
2024-10-31 20:38:43 +0900

    [Feat] 회원가입 및 로그인 구현

--- src/main/java/org/sopt/diary/api/UserController.java
+++ src/main/java/org/sopt/diary/api/UserController.java
@@ -1,13 +1,46 @@
 package org.sopt.diary.api;
 
+import org.sopt.diary.api.dto.user.request.SignInRequest;
+import org.sopt.diary.api.dto.user.request.SignUpRequest;
+import org.sopt.diary.api.dto.user.response.SignInResponse;
+import org.sopt.diary.service.User;
 import org.sopt.diary.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public void signUp(@RequestBody SignUpRequest signUpRequest) {
+        userService.signUp(
+                new User(
+                        null,
+                        signUpRequest.getLoginId(),
+                        signUpRequest.getPassword(),
+                        signUpRequest.getNickname()
+                )
+        );
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest signInRequest) {
+        User user = userService.signIn(
+                signInRequest.getLoginId(),
+                signInRequest.getPassword()
+        );
+
+        SignInResponse signInResponse = new SignInResponse(user.getId());
+
+        return ResponseEntity.ok(signInResponse);
     }
 }
--- src/main/java/org/sopt/diary/repository/UserRepository.java
+++ src/main/java/org/sopt/diary/repository/UserRepository.java
@@ -3,6 +3,9 @@ package org.sopt.diary.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserEntityByLoginIdAndPassword(String id, String password);
 }
